ff5a8df25ccdeeb31d7c53ec4938cdb9f667a323
rafifkuspardono <163708909+rafifkuspardono@users.noreply.github.com>
2025-05-23 00:14:41 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
-import "fmt"
-
-type bahanMakanan struct {
-	nama                                                   string
-	jumlahStok                                             int
-	tanggalKadaluwarsa, bulanKadaluwarsa, tahunKadaluwarsa int
-	sudahDipakai                                           bool
-}
-
-type tabMakanan [100]bahanMakanan
+import (
+	"TubesAlpro/manajemenData"
+	"fmt"
+)
 
 func main() {
 	var pilihan, nData int
-	var makanan tabMakanan
+	var makanan manajemenData.TabMakanan
 
 	for {
 		menu()
@@ -21,10 +15,8 @@ func main() {
 		fmt.Scan(&pilihan)
 
 		switch pilihan {
-		case 1:
-			printData(makanan, nData)
 		case 2:
-			tambahData(&makanan, &nData)
+			manajemenData.TambahData(&makanan, &nData)
 		}
 
 	}
@@ -35,24 +27,4 @@ func menu() {
 	fmt.Println("1. Daftar Bahan Makanan")
 	fmt.Println("2. Tambah Data Makanan")
 	fmt.Println("0. KELUAR\n")
-}
-
-func tambahData(A *tabMakanan, n *int) {
-	var i, batas int
-	fmt.Println("\nJumlah makanan yang ingin ditambahkan: ")
-	fmt.Scan(&batas)
-	for i = 0; i < batas; i++ {
-		fmt.Println("Masukkan nama makanan: ")
-		fmt.Scan(&A[*n].nama)
-		fmt.Println("Masukkan jumlah Stok: ")
-		fmt.Scan(&A[*n].jumlahStok)
-		fmt.Println("Masukkan tanggal kadaluwarsa: ")
-		fmt.Scan(&A[*n].tanggalKadaluwarsa)
-		fmt.Println("Masukkan bulan kadaluwarsa: ")
-		fmt.Scan(&A[*n].bulanKadaluwarsa)
-		fmt.Println("Masukkan tahun kadaluwarsa: ")
-		fmt.Scan(&A[*n].tahunKadaluwarsa)
-		fmt.Println("Data makanan berhasil ditambahkan")
-		*n = *n + 1
-	}
 }
