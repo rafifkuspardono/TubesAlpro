3aea155002d28cce35b0401c8f94120d852639f1
rafifkuspardono <163708909+rafifkuspardono@users.noreply.github.com>
2025-06-07 19:36:35 +0700

    Update binary.go

--- pencarian/binary.go
+++ pencarian/binary.go
@@ -2,9 +2,11 @@ package pencarian
 
 import (
 	"TubesAlpro/manajemenData"
+	"TubesAlpro/pengurutan"
+	"fmt"
 )
 
-func BinarySearch(T manajemenData.TabMakanan, n int, X string) int {
+func BinarySearch(A manajemenData.TabMakanan, n int, X string) int {
 	var left, right, mid int
 	var found int = -1
 	left = 0
@@ -12,13 +14,32 @@ func BinarySearch(T manajemenData.TabMakanan, n int, X string) int {
 
 	for left <= right && found == -1 {
 		mid = (left + right) / 2
-		if X < T[mid].Nama {
+		if X < A[mid].Nama {
 			right = mid - 1
-		} else if X > T[mid].Nama {
+		} else if X > A[mid].Nama {
 			left = mid + 1
 		} else {
 			found = mid
 		}
 	}
 	return found
+}
+
+func CariBinarySearch(A *manajemenData.TabMakanan, n int) {
+	pengurutan.UrutkanNamaMakanan(A, n)
+
+	var x string
+	fmt.Print("Masukkan nama bahan yang dicari: ")
+	fmt.Scan(&x)
+
+	var idx int
+	idx = BinarySearch(*A, n, x)
+
+	if idx != -1 {
+		fmt.Printf("Data ditemukan di index ke-%d:\n", idx)
+		fmt.Printf("%s - %d buah - Kadaluarsa: %02d-%02d-%04d - Dipakai: %v\n",
+			A[idx].Nama, A[idx].JumlahStok, A[idx].TanggalKadaluwarsa, A[idx].BulanKadaluwarsa, A[idx].TahunKadaluwarsa, A[idx].SudahDipakai)
+	} else {
+		fmt.Println("Data tidak ditemukan.")
+	}
 }
